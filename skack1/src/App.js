d7de034bfc4fa329587a9805a67947716653794b
FavourToAll <137409516+FavourToAll@users.noreply.github.com>
2023-06-27 19:07:17 +0530

    Add files via upload

---
+++ skack1/src/App.js
@@ -0,0 +1,31 @@
+ import { BrowserRouter as Router } from 'react-router-dom'
+ import React, { useEffect } from 'react'
+ import { useDispatch } from 'react-redux';
+ 
+ 
+ 
+ import './App.css';
+ import Navbar from './components/Navbar/Navbar'
+ import AllRoutes from  './AllRoutes'
+ import { fetchAllQuestions } from './actions/question';
+  function App() {
+    
+     const dispatch = useDispatch()
+
+    useEffect(() => {
+      dispatch(fetchAllQuestions())
+    }, [dispatch])
+
+
+   return (
+    <div className="App">
+      <Router >
+       <Navbar />
+       <AllRoutes />
+       </Router >
+      
+    </div>
+  );
+}
+
+export default App;
